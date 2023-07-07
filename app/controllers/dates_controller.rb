f2e9bd8e744987efa6e1a5197e5f200b32ebab27
Aakash Soni <inos92@gmail.com>
2023-07-06 20:48:43 -0500

    created GET req for historical event API in controller, encrypted API key

--- app/controllers/dates_controller.rb
+++ app/controllers/dates_controller.rb
@@ -1,2 +1,17 @@
 class DatesController < ApplicationController
+  def index
+    url = URI("https://historical-events-by-api-ninjas.p.rapidapi.com/v1/historicalevents?month=3&day=30")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request["X-RapidAPI-Key"] = "#{Rails.application.credentials.history_api[:api_key]}"
+    request["X-RapidAPI-Host"] = "historical-events-by-api-ninjas.p.rapidapi.com"
+
+    response = http.request(request)
+    # render json: response.as_json
+
+    render json: response.body
+  end
 end
